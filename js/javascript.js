67c2b188dd2371275dbc822f1d688ef5ab9f458a
MrSunshyne <s4ndeep1203@gmail.com>
2013-01-27 13:42:56 +0400

    added css3 effects and menu

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-  
+  new MenuController();
 })
+
+
+function MenuController(){
+	var me = this;
+	links = $('.main-menu a');	
+	$('article').fadeOut();
+	links.each(function(){			
+		$(this).bind('click', { link : $(this)}, function(e){
+			e.preventDefault();
+			$('.loadstate').removeClass('loadstate');
+			if ($(this).parent().hasClass('active')){
+
+			}else{
+				$('.active').removeClass('active');
+				$('article').fadeOut();
+				e.data.link.parent().addClass('active');
+				var id = e.data.link.attr('href');
+				$(id).fadeIn();
+			}
+		});
+			
+	})
+
+	//me.togglemenu = 
+
+}
