e9783f6d8cc97753f584cef5e7b4795f125e29e9
Sandeep Ramgolam <s4ndeep1203@gmail.com>
2013-05-30 12:10:45 +0400

    Added Github contributions  using Github API

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   new MenuController();
+  $('[href="#portfolio"]').bind('click', getGithubFeed);
+
 })
 
 
@@ -35,7 +37,28 @@ function MenuController(){
 		$('.active').removeClass('active');
 		$('.poster-card-wrapper').show();
 	})
+}
 
+function getGithubFeed(){
+	if($('#github-feed').html() != "Loading..."){
+		return false;
+	}
 
+	$("#github-feed").html('');
+    $.ajax({
+        type: "GET",
+        url: "https://api.github.com/users/mrsunshyne/repos",
+        dataType: "json",
+        success: function(result) {
+            for( i in result ) {
+                $("#github-feed").append(
+                    "<li><h2><a href='" + result[i].html_url + "' target='_blank'>" + result[i].name + "</a></h2><p>" + result[i].description + "</p></li>"
+                );
+                console.log("i: " + i);
+            }
+            console.log(result);
+            $("#github-feed").append("<h2>Total Repos: " + result.length+ "</h2>");
+        }
+    });
 
 }
