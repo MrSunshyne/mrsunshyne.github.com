b426eeb799935b04ff974246e6eb1fe183f18b9b
Sandeep Ramgolam <sandeep@ramgolam.com>
2020-04-23 00:31:26 +0400

    update github page finally after almost 5 years

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="flex min-h-screen flex-col bg-gray-200">
+		<router-view class="container mx-auto flex-grow" />
+	</div>
+</template>
+
+<script>
+import MainMenu from "./components/main-menu.vue";
+import FooterSection from "./components/footer-section.vue";
+
+export default {
+	components: {
+		MainMenu,
+		FooterSection
+	}
+};
+</script>
+
+<style lang="scss">
+* {
+	padding: 0;
+	border: 0;
+	margin: 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="home flex flex-col justify-center items-center text-center">
+		<h1 class="text-3xl">Sandeep Ramgolam</h1>
+		<p class="text-xl py-3 px-2">
+			Hello, my personal website has moved to
+			<span class="font-bold">sandeep.ramgolam.com</span>
+		</p>
+		<a
+			href="https://sandeep.ramgolam.com"
+			class="p-3 bg-gray-700 text-white rounded-lg my-3 uppercase text-xs hover:bg-gray-800"
+		>Go to site</a>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "home",
+	components: {}
+};
+</script>
